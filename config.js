2a48e5ddeccd92cb54b053fdbd5175e515cc0096
Jesús Ibrahim Haros Cienfuegos <ibrahimharos@gmail.com>
2025-09-01 15:39:13 -0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Configuración de Supabase - REEMPLAZA ESTOS VALORES CON LOS DE TU PROYECTO
-window.SUPABASE_CONFIG = {
-    url: 'https://genidguluqumknwtoqfm.supabase.co', // Reemplaza con tu URL de Supabase
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlbmlkZ3VsdXF1bWtud3RvcWZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MjY2MTQsImV4cCI6MjA2ODEwMjYxNH0.GNG_V-rkQWamqqyX4T1tWoh1hLEac_nJv40JX5o63WY' // Reemplaza con tu clave anónima pública
-};
